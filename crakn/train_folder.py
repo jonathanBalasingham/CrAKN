c97facbc144935233ab52e2117c78277a72d007e
jonathanBalasingham <jonbalasin@gmail.com>
2024-05-09 15:33:19 +0100

    seperate logic

--- crakn/train_folder.py
+++ crakn/train_folder.py
@@ -44,12 +44,57 @@ parser.add_argument(
 )
 
 
-def train_folder(config: TrainingConfig, folder_path: str, cache: bool):
+def read_folder(folder_path: str):
     id_prop_file = os.path.join(folder_path, "id_prop.csv")
     with open(id_prop_file, "r") as f:
         reader = csv.reader(f)
         data = [row for row in reader]
 
+    files = os.listdir(folder_path)
+    for file in files:
+        ext = os.path.basename(file).split(".")[-1]
+        if ext == "csv":
+            pass
+        else:
+            file_format = ext
+            break
+
+    print(f"Using file format: {file_format}")
+    structures = []
+    ids = []
+    targets = []
+
+    for i in tqdm(data, desc="Reading files from folders.."):
+        file_name = i[0]
+        if not file_name.endswith(file_format):
+            file_name += f".{file_format}"
+        file_path = os.path.join(folder_path, file_name)
+        if file_format == "poscar":
+            atoms = Atoms.from_poscar(file_path)
+        elif file_format == "cif":
+            atoms = Atoms.from_cif(file_path, use_cif2cell=False)
+        elif file_format == "xyz":
+            atoms = Atoms.from_xyz(file_path, box_size=500)
+        elif file_format == "pdb":
+            atoms = Atoms.from_pdb(file_path, max_lat=500)
+        else:
+            raise NotImplementedError(
+                "File format not implemented", file_format
+            )
+
+        if DATA_FORMATS[config.base_config.backbone] == "pymatgen":
+            structure = atoms.pymatgen_converter()
+            structures.append(structure)
+        elif DATA_FORMATS[config.base_config.backbone] == "jarvis":
+            structures.append(atoms)
+
+        ids.append(file_name)
+        tmp = [float(j) for j in i[1:]]
+        targets.append(tmp)
+
+    return structures, targets, ids
+
+def train_folder(config: TrainingConfig, folder_path: str, cache: bool):
     cache_path = os.path.join(folder_path, "cache_structures")
     dataloader_path = os.path.join(folder_path, "dataloaders")
     if os.path.exists(dataloader_path) and cache:
@@ -58,49 +103,7 @@ def train_folder(config: TrainingConfig, folder_path: str, cache: bool):
         history = train_crakn(config, dataloaders=(train_loader, val_loader, test_loader))
         return history
     else:
-        files = os.listdir(folder_path)
-        for file in files:
-            ext = os.path.basename(file).split(".")[-1]
-            if ext == "csv":
-                pass
-            else:
-                file_format = ext
-                break
-
-        print(f"Using file format: {file_format}")
-        structures = []
-        ids = []
-        targets = []
-
-        for i in tqdm(data, desc="Reading files from folders.."):
-            file_name = i[0]
-            if not file_name.endswith(file_format):
-                file_name += f".{file_format}"
-            file_path = os.path.join(folder_path, file_name)
-            if file_format == "poscar":
-                atoms = Atoms.from_poscar(file_path)
-            elif file_format == "cif":
-                atoms = Atoms.from_cif(file_path, use_cif2cell=False)
-            elif file_format == "xyz":
-                atoms = Atoms.from_xyz(file_path, box_size=500)
-            elif file_format == "pdb":
-                atoms = Atoms.from_pdb(file_path, max_lat=500)
-            else:
-                raise NotImplementedError(
-                    "File format not implemented", file_format
-                )
-
-            if DATA_FORMATS[config.base_config.backbone] == "pymatgen":
-                structure = atoms.pymatgen_converter()
-                structures.append(structure)
-            elif DATA_FORMATS[config.base_config.backbone] == "jarvis":
-                structures.append(atoms)
-
-            ids.append(file_name)
-
-            tmp = [float(j) for j in i[1:]]
-            targets.append(tmp)
-
+        structures, targets, ids = read_folder(folder_path)
         pickle.dump((structures, targets, ids), open(cache_path, "wb"))
 
     dataset = CrAKNDataset(structures, targets, ids, config)
